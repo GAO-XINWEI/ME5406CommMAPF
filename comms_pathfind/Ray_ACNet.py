35ef226649cbd30c25d49e0c4bdf588526b128ea
classic-byte <79648870+classic-byte@users.noreply.github.com>
2021-11-05 22:30:06 +0800

    Add files via upload

--- comms_pathfind/Ray_ACNet.py
+++ comms_pathfind/Ray_ACNet.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 import tensorflow.contrib.layers as layers
 import numpy as np
+
 #parameters for training
-GRAD_CLIP              = 1000.0
-KEEP_PROB1             = 1 # was 0.5
-KEEP_PROB2             = 1 # was 0.7
-RNN_SIZE               = 512
-GOAL_REPR_SIZE         = 12
+GRAD_CLIP = 10.0
+KEEP_PROB1 = 1  # was 0.5
+KEEP_PROB2 = 1  # was 0.7
+RNN_SIZE = 512
+GOAL_REPR_SIZE = 12
+
 
 #Used to initialize weights for policy and value output layers (Do we need to use that? Maybe not now)
 def normalized_columns_initializer(std=1.0):
@@ -14,16 +16,20 @@ def normalized_columns_initializer(std=1.0):
         out = np.random.randn(*shape).astype(np.float32)
         out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
         return tf.constant(out)
+
     return _initializer
 
+
 class ACNet:
-    def __init__(self, scope, a_size, trainer,TRAINING,GRID_SIZE,GLOBAL_NET_SCOPE,GLOBAL_NETWORK=False):
+    def __init__(self, scope, a_size, trainer,TRAINING, NUM_CHANNEL, OBS_SIZE, GLOBAL_NET_SCOPE, GLOBAL_NETWORK=False):
         with tf.variable_scope(str(scope)+'/qvalues'):
+            self.trainer = trainer
             #The input size may require more work to fit the interface.
-            self.inputs = tf.placeholder(shape=[None,5,GRID_SIZE,GRID_SIZE], dtype=tf.float32)
+            self.inputs = tf.placeholder(shape=[None,5, OBS_SIZE, OBS_SIZE], dtype=tf.float32)
             self.goal_pos=tf.placeholder(shape=[None,3],dtype=tf.float32)
             self.myinput = tf.transpose(self.inputs, perm=[0,2,3,1])
-            self.policy, self.value, self.state_out, self.state_in, self.state_init, self.blocking, self.on_goal, self.mean_goal, self.message, self.valids = self._build_net(self.myinput,self.goal_pos,RNN_SIZE,TRAINING,a_size)
+            self.policy, self.value, self.state_out, self.state_in, self.state_init, self.blocking, self.mean_goal, self.message, self.valids = self._build_net(
+                self.myinput, self.goal_pos, RNN_SIZE, TRAINING, a_size)
         if TRAINING:
             self.actions                = tf.placeholder(shape=[None], dtype=tf.int32)
             self.actions_onehot         = tf.one_hot(self.actions, a_size, dtype=tf.float32)
@@ -39,21 +45,18 @@ class ACNet:
             self.optimal_actions        = tf.placeholder(tf.int32,[None])
             self.optimal_actions_onehot = tf.one_hot(self.optimal_actions, a_size, dtype=tf.float32)
 
-            
             # Loss Functions
-            self.value_loss    = tf.reduce_sum(self.train_value*tf.square(self.target_v - tf.reshape(self.value, shape=[-1])))
+            self.value_loss    = 0.1 * tf.reduce_sum(self.train_value*tf.square(self.target_v - tf.reshape(self.value, shape=[-1])))
             self.entropy       = - tf.reduce_sum(self.policy * tf.log(tf.clip_by_value(self.policy,1e-10,1.0)))
-            self.policy_loss   = - tf.reduce_sum(tf.log(tf.clip_by_value(self.responsible_outputs,1e-15,1.0)) * self.advantages)
+            self.policy_loss   = -0.5 * tf.reduce_sum(tf.log(tf.clip_by_value(self.responsible_outputs,1e-15,1.0)) * self.advantages)
             self.valid_loss    = - tf.reduce_sum(tf.log(tf.clip_by_value(self.valids,1e-10,1.0)) *\
                                 self.train_valid+tf.log(tf.clip_by_value(1-self.valids,1e-10,1.0)) * (1-self.train_valid))
             self.blocking_loss = - tf.reduce_sum(self.target_blockings*tf.log(tf.clip_by_value(self.blocking,1e-10,1.0))\
                                       +(1-self.target_blockings)*tf.log(tf.clip_by_value(1-self.blocking,1e-10,1.0)))
-            self.on_goal_loss = - tf.reduce_sum(self.target_on_goals*tf.log(tf.clip_by_value(self.on_goal,1e-10,1.0))\
-                                      +(1-self.target_on_goals)*tf.log(tf.clip_by_value(1-self.on_goal,1e-10,1.0)))
             self.mean_goal_loss = - tf.reduce_sum(self.target_meangoals * tf.log(tf.clip_by_value(self.mean_goal, 1e-10, 1.0)) \
                                       + (1 - self.target_meangoals) * tf.log(tf.clip_by_value(1 - self.mean_goal, 1e-10, 1.0)))
             self.message_loss = - tf.reduce_sum(self. advantages * tf.log(tf.clip_by_value(self.message, 1e-10, 1.0)))  ## todo: debug
-            self.loss          = 0.5 * self.value_loss + self.policy_loss + 0.5 * self.mean_goal_loss + 0.5 * self.message_loss + 0.5*self.valid_loss \
+            self.loss          = self.value_loss + self.policy_loss + 0.5 * self.mean_goal_loss + 0.5 * self.message_loss + 0.5*self.valid_loss \
                             - self.entropy * 0.01 +.5*self.blocking_loss
             # self.imitation_loss        = 0.5 * self.value_loss + self.policy_loss + 0.5*self.valid_loss - self.entropy * 0.01 +.5*self.blocking_loss
             # self.imitation_loss = tf.reduce_mean(tf.contrib.keras.backend.categorical_crossentropy(self.optimal_actions_onehot,self.policy))
@@ -63,20 +66,33 @@ class ACNet:
             local_vars         = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope+'/qvalues')
             self.gradients     = tf.gradients(self.loss, local_vars)
             self.var_norms     = tf.global_norm(local_vars)
-            grads, self.grad_norms = tf.clip_by_global_norm(self.gradients, GRAD_CLIP)
+            self.grads, self.grad_norms = tf.clip_by_global_norm(self.gradients, GRAD_CLIP)
 
             # Apply local gradients to global network
             global_vars        = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, GLOBAL_NET_SCOPE+'/qvalues')
-            self.apply_grads   = trainer.apply_gradients(zip(grads, global_vars))
+            if self.trainer:
+                self.apply_grads   = self.trainer.apply_gradients(zip(self.grads, global_vars))
+
+            self.local_vars = local_vars
 
             #now the gradients for imitation loss
             self.i_gradients     = tf.gradients(self.imitation_loss, local_vars)
             self.i_var_norms     = tf.global_norm(local_vars)
-            i_grads, self.i_grad_norms = tf.clip_by_global_norm(self.i_gradients, GRAD_CLIP)
+            self.i_grads, self.i_grad_norms = tf.clip_by_global_norm(self.i_gradients, GRAD_CLIP)
 
             # Apply local gradients to global network
-            self.apply_imitation_grads   = trainer.apply_gradients(zip(i_grads, global_vars))
-        print("Hello World... From  "+str(scope))     # :)
+            if self.trainer:
+                self.apply_imitation_grads   = trainer.apply_gradients(zip(self.i_grads, global_vars))
+        # print("Hello World... From  "+str(scope))     # :)
+
+        if GLOBAL_NETWORK:
+            print("\n\n\n\n is a global network\n\n\n\n")
+            weightVars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+            self.tempGradients = [tf.placeholder(shape=w.get_shape(), dtype=tf.float32) for w in weightVars]
+            self.apply_grads = self.trainer.apply_gradients(zip(self.tempGradients, weightVars))
+            # self.clippedGrads, norms = tf.clip_by_global_norm(self.tempGradients, GRAD_CLIP)
+            # self.apply_grads = self.trainer.apply_gradients(zip(self.clippedGrads, weightVars))
+        print("Hello World... From  " + str(scope))  # :)
 
     def _build_net(self,inputs,goal_pos,RNN_SIZE,TRAINING,a_size):
         w_init   = layers.variance_scaling_initializer()
@@ -111,8 +127,8 @@ class ACNet:
         step_size = tf.shape(inputs)[:1]
         state_in = tf.nn.rnn_cell.LSTMStateTuple(c_in, h_in)
         lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
-        lstm_cell, rnn_in, initial_state=state_in, sequence_length=step_size,
-        time_major=False)
+            lstm_cell, rnn_in, initial_state=state_in, sequence_length=step_size,
+            time_major=False)
         lstm_c, lstm_h = lstm_state
         state_out = (lstm_c[:1, :], lstm_h[:1, :])
         self.rnn_out = tf.reshape(lstm_outputs, [-1, RNN_SIZE])
@@ -122,8 +138,8 @@ class ACNet:
         policy_sig   = tf.sigmoid(policy_layer)
         value        = layers.fully_connected(inputs=self.rnn_out, num_outputs=1, weights_initializer=normalized_columns_initializer(1.0), biases_initializer=None, activation_fn=None)
         blocking      = layers.fully_connected(inputs=self.rnn_out, num_outputs=1, weights_initializer=normalized_columns_initializer(1.0), biases_initializer=None, activation_fn=tf.sigmoid)
-        on_goal      = layers.fully_connected(inputs=self.rnn_out, num_outputs=1, weights_initializer=normalized_columns_initializer(1.0), biases_initializer=None, activation_fn=tf.sigmoid)
+        # on_goal      = layers.fully_connected(inputs=self.rnn_out, num_outputs=1, weights_initializer=normalized_columns_initializer(1.0), biases_initializer=None, activation_fn=tf.sigmoid)
         mean_goal    = layers.fully_connected(inputs=self.rnn_out, num_outputs=1, weights_initializer=normalized_columns_initializer(1.0), biases_initializer=None, activation_fn=tf.sigmoid)
         message      = layers.fully_connected(inputs=self.rnn_out, num_outputs=1, weights_initializer=normalized_columns_initializer(1.0), biases_initializer=None, activation_fn=tf.sigmoid)
 
-        return policy, value, state_out ,state_in, state_init, blocking, on_goal, mean_goal, message, policy_sig
+        return policy, value, state_out ,state_in, state_init, blocking, mean_goal, message, policy_sig
